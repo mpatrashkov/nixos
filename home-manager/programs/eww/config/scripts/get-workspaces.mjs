62a1c1bc91bec066613f3eeb5628998f89114ecc
Miroslav Patrashkov <mpatrashkov2002@outlook.com>
2024-01-25 12:14:01 +0200

    setup eww workspaces

--- home-manager/programs/eww/config/scripts/get-workspaces.mjs
+++ home-manager/programs/eww/config/scripts/get-workspaces.mjs
@@ -5,13 +5,42 @@
 
 $.verbose = false;
 
-const workspaces = JSON.parse(await $`hyprctl workspaces -j`);
+import net from "net";
 
-console.log(
-  JSON.stringify(
-    workspaces.map((workspace) => ({
-      id: workspace.id.toString(),
-      windows: workspace.windows,
-    }))
+async function getWorkspaces() {
+  const workspaces = JSON.parse(await $`hyprctl workspaces -j`);
+
+  console.log(
+    JSON.stringify(
+      workspaces
+        .map((workspace) => ({
+          id: workspace.id.toString(),
+          windows: workspace.windows,
+        }))
+        .sort((a, b) => a.id - b.id)
+    )
+  );
+}
+
+await getWorkspaces();
+
+const connection = net
+  .createConnection(
+    `/tmp/hypr/${process.env.HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock`
   )
-);
+  .on("data", (data) => {
+    getWorkspaces();
+  })
+  .on("error", (error) => {
+    console.log(error);
+  });
+
+let SHUTDOWN = false;
+function cleanup() {
+  if (!SHUTDOWN) {
+    SHUTDOWN = true;
+    connection.end();
+    process.exit(0);
+  }
+}
+process.on("SIGINT", cleanup);
