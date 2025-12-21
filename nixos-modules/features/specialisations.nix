c58c1bfd2d8538812fab0bb40eeb897aff281e43
Miroslav Patrashkov <mpatrashkov2002@outlook.com>
2025-12-21 16:23:25 +0200

    remove separate gnome specialisation

--- nixos-modules/features/specialisations.nix
+++ nixos-modules/features/specialisations.nix
@@ -1,11 +1,11 @@
 { ... }:
 {
-  # Default specialisation
-  myNixOS.services.gnome.enable = false;
+  # Do not forget to disable optional features in the base specialisation
+  # myNixOS.services.hyprland.enable = false;
 
-  config.specialisation = {
-    gnome.configuration = {
-      myNixOS.services.gnome.enable = true;
-    };
-  };
+  # config.specialisation = {
+  #   hyprland.configuration = {
+  #     myNixOS.services.hyprland.enable = true;
+  #   };
+  # };
 }
