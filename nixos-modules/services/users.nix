3d8acdb1fad8e873b4203b05a3c76b390fc8257c
Miroslav Patrashkov <mpatrashkov2002@outlook.com>
2026-03-18 19:34:21 +0200

    move users config into a separate file

---
+++ nixos-modules/services/users.nix
@@ -0,0 +1,20 @@
+{ ... }:
+
+{
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.miro = {
+    isNormalUser = true;
+    description = "Miroslav Patrashkov";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "libvirtd"
+      "docker"
+      "input"
+      "kvm"
+      "adbusers"
+      "i2c"
+      "dialout"
+    ];
+  };
+}
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,23 +19,6 @@
     "electron-25.9.0"
   ];
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.miro = {
-    isNormalUser = true;
-    description = "Miroslav Patrashkov";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-      "libvirtd"
-      "docker"
-      "input"
-      "kvm"
-      "adbusers"
-      "i2c"
-      "dialout"
-    ];
-  };
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
