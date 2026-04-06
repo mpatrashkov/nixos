183b2651dd01ed32760a5f1ce769efaed79a2049
Miroslav Patrashkov <mpatrashkov2002@outlook.com>
2026-04-06 15:58:43 +0300

    use static ip

--- nixos-modules/services/networking.nix
+++ nixos-modules/services/networking.nix
@@ -6,6 +6,13 @@
       "127.0.0.1"
       "::1"
     ];
+    defaultGateway = "192.168.100.1";
+    interfaces.enp6s0.ipv4.addresses = [
+      {
+        address = "192.168.100.2";
+        prefixLength = 24;
+      }
+    ];
   };
 
   networking.firewall = {
