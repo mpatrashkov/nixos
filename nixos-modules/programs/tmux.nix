cacbce984a847d502057f78a85717811c315173d
Miroslav Patrashkov <mpatrashkov2002@outlook.com>
2026-05-03 21:08:23 +0300

    add tmux plugin

--- nixos-modules/programs/tmux.nix
+++ nixos-modules/programs/tmux.nix
@@ -1,9 +1,28 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  tmux-fzf-links = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "fzf-links";
+    version = "1.4.15";
+    src = pkgs.fetchFromGitHub {
+      owner = "alberti42";
+      repo = "tmux-fzf-links";
+      rev = "1.4.15";
+      hash = "sha256-ZAZNOBE4n7tXpszNFw6Ri8BlV9s/4x9H2NovqRmOrCY=";
+    };
+    rtpFilePath = "fzf-links.tmux";
+  };
+in
 {
   programs.tmux = {
     enable = true;
     baseIndex = 1;
+    plugins = [ tmux-fzf-links ];
+    extraConfigBeforePlugins = ''
+      set-option -g @fzf-links-python "${pkgs.python3}/bin/python3"
+      set-option -g @fzf-links-fzf-path "${pkgs.fzf}/bin/fzf"
+      set-option -g @fzf-links-use-colors on
+    '';
     extraConfig = ''
       unbind C-b
       set-option -g prefix M-a
