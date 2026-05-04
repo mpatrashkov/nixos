bd821ac017fd33a29053eb6a2c893829bb67e5b5
Miroslav Patrashkov <mpatrashkov2002@outlook.com>
2026-05-04 18:00:38 +0300

    remove hm modules and add stow

--- home-manager-modules/features/fastfetch/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, ... }:
-
-{
-  xdg.configFile."fastfetch/config.jsonc".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/flake/home-manager-modules/features/fastfetch/config.jsonc";
-
-  xdg.configFile."fastfetch/short-config.jsonc".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/flake/home-manager-modules/features/fastfetch/short-config.jsonc";
-}
--- home-manager-modules/features/hyprlock/default.nix
+++
@@ -1,6 +0,0 @@
-{ config, ... }: {
-  programs.hyprlock.enable = true;
-
-  xdg.configFile."hypr/hyprlock.conf".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/flake/home-manager-modules/features/hyprlock/hyprlock.conf";
-}
